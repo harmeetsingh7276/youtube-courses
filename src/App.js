38263cb9cbd8cdc7bec01479d9f1b793ef10fec5
harmeet.gorowada <harmeet.gorowada@techbulls.com>
2022-09-13 20:09:50 +0530

    bugs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
+import React, { Component }  from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 import './App.css';
 import Header from './components/Header';
+import {Button} from "reactstrap";
 
 function App() {
   return (
     <div>
-      <Header name="first Header" title="first card"/>
-      <hr/>
-      <Header name="second Header" title="second card"/>
-      <h1>React JS</h1>
-      <p>First Para</p>
+      <h1>BootStrap components</h1>
+      <Button color='primary'>First React Button</Button>
     </div>
   );
 }
