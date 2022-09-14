a5833fc4d7cfededbe4b0094aba352dd1eb2121d
harmeet.gorowada <harmeet.gorowada@techbulls.com>
2022-09-14 15:28:50 +0530

    Linking the backend code with the frontend for Adding course

--- src/components/AddCourse.js
+++ src/components/AddCourse.js
@@ -1,26 +1,57 @@
-import React, { Fragment,useEffect, useState } from "react";
-import {Button, Container, Form, FormGroup, Input} from "reactstrap";
+import React, { Fragment, useEffect, useState } from "react";
+import { Button, Container, Form, FormGroup, Input } from "reactstrap";
+import axios from "axios";
+import base_url from "../api/bootapi";
 
-const AddCourse=()=>{
-    useEffect(()=>{
-        document.title="Add Course";
-    },[])
-    const[course,setCourse]=useState({});
-    return(
+const AddCourse = () => {
+    useEffect(() => {
+        document.title = "Add Course";
+    }, [])
+    const [course, setCourse] = useState({});
+
+    //form handler function
+    const handleForm = (e) => {
+        console.log(course);
+        postDataToServer(course);
+        e.preventDefault();
+    };
+
+
+    //function to post data on server
+    const postDataToServer=(data)=>{
+        axios.post(`${base_url}/courses`,data).then(
+            (response)=>{
+                console.log(response);
+                console.log("success");
+            },(error)=>{
+                console.log(error);
+                console.log("error");
+            }
+        )
+    };
+
+
+    return (
         <Fragment>
             <h1>Fill Course Details</h1>
-            <Form>
+            <Form onSubmit={handleForm}>
                 <FormGroup>
                     <label for="userId">Course Id</label>
-                    <Input type="text" placeholder="Enter here" name="userId" id="userId"/>
+                    <Input type="text" placeholder="Enter here" name="userId" id="userId" onChange={(e) => {
+                        setCourse({ ...course, id: e.target.value })
+                    }} />
                 </FormGroup>
                 <FormGroup>
                     <label for="title">Course Title</label>
-                    <Input type="text" placeholder="Enter title here" name="title" id="title"/>
+                    <Input type="text" placeholder="Enter title here" name="title" id="title" onChange={(e) => {
+                        setCourse({ ...course, title: e.target.value })
+                    }} />
                 </FormGroup>
                 <FormGroup>
                     <label for="description">Course Description</label>
-                    <Input type="textarea" placeholder="Enter description here" name="description" id="description" style={{height:120}}/>
+                    <Input type="textarea" placeholder="Enter description here" name="description" id="description" style={{ height: 120 }} onChange={(e) => {
+                        setCourse({ ...course, description: e.target.value })
+                    }} />
                 </FormGroup>
                 <Container className="text-center">
                     <Button color="success" type="submit">Add Course</Button>
